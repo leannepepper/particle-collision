86a753a84ea09d0ec9fc2d5f5a0c428eb26035df
Leanne Werner <leanne.werner.lw@gmail.com>
2021-12-01 09:20:21 -0800

    config

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge')
 const commonConfiguration = require('./webpack.common.js')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path')
 
 module.exports = merge(commonConfiguration, {
   mode: 'production',
