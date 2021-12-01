2b70e876b8e89ca23139a09faa8dc6607ff9d333
Leanne Werner <leanne.werner.lw@gmail.com>
2021-12-01 09:09:10 -0800

    config

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -2,13 +2,12 @@ const { merge } = require('webpack-merge')
 const commonConfiguration = require('./webpack.common.js')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
-module.exports = merge(
-    commonConfiguration,
-    {
-        mode: 'production',
-        plugins:
-        [
-            new CleanWebpackPlugin()
-        ]
-    }
-)
+module.exports = merge(commonConfiguration, {
+  mode: 'production',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/particle-collision'
+  },
+  plugins: [new CleanWebpackPlugin()]
+})
